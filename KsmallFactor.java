ee9a27b82e5940395caa8de0627e9080c4e0f503
motumag <motumagishu27@gmail.com>
2023-07-14 21:23:26 +0300

    first commit

---
+++ KsmallFactor.java
@@ -0,0 +1,25 @@
+public class KsmallFactor {
+    public static boolean hasKSmallFactors(int k, int n) {
+        if (k <= 0 || n <= 0) {
+            return false;
+        }
+        for (int u = 1; u < k; u++) {
+            if (n % u == 0) {
+                int v = n / u;
+                if (v < k) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasKSmallFactors(10, 20));
+        System.out.println(hasKSmallFactors(6, 20));
+        System.out.println(hasKSmallFactors(7, 20));
+        System.out.println(hasKSmallFactors(8, 20));
+    }
+
+}
